e62244b4e25d659f0badbdfa50d6a49276c9fa94
chenbd <chenbd@win-star.com>
2018-12-15 16:17:09 +0800

    yes

---
+++ ant-design-pro-vue-master/src/views/account/center/page/Article.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-list>
+    <a-list-item>
+			文章
+    </a-list-item>
+  </a-list>
+</template>
+
+<script>
+  import AList from 'ant-design-vue/es/list'
+  import AListItem from 'ant-design-vue/es/list/Item'
+
+  export default {
+    name: "Article",
+    components: {
+      AList,
+      AListItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ ant-design-pro-vue-master/src/views/account/center/page/Project.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-list>
+    <a-list-item>
+			项目
+    </a-list-item>
+  </a-list>
+</template>
+
+<script>
+  export default {
+    name: "Project"
+  }
+</script>
+
+<style scoped>
+
+</style>
